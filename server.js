2ddeffc3733f2d28ef0e7fe22e2192b1ea35fa85
Mikołaj Pęcak <mikolajpecak@gmail.com>
2019-09-27 21:57:51 +0200

    Improve encryption

--- server.js
+++ server.js
@@ -18,9 +18,12 @@ if (process.env.PRODUCTION === "true") {
 
 function runServer(server) {
   io(server).on("connection", function(socket) {
-    socket.on("chat_room", function(data) {
-      socket.join(data.roomId);
-      socket.to(data.roomId).broadcast.emit("chat_online", data.key);
+    socket.on("chat_room", function(roomId) {
+      socket.join(roomId);
+      socket.to(roomId).broadcast.emit("chat_online");
+    });
+    socket.on("chat_key", function(data) {
+      socket.to(data.roomId).broadcast.emit("chat_key", data.key);
     });
     socket.on("chat_message", function(data) {
       socket.to(data.roomId).broadcast.emit("chat_message", data.message);
